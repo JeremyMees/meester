8499dfdb432256f2190e74149e10292f8e6ffd1e
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-05-10 17:11:26 +0200

    fix: removed unused env variable

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -40,12 +40,6 @@ export default defineNuxtConfig({
     projectId: process.env.NUXT_SANITY_PROJECT_ID,
     dataset: process.env.NUXT_SANITY_DATASET,
     apiVersion: process.env.NUXT_SANITY_API_VERSION || '2025-10-20',
-    token: process.env.NUXT_SANITY_API_READ_TOKEN,
-    ignoreBrowserTokenWarning: true,
-    visualEditing: {
-      token: process.env.NUXT_SANITY_API_READ_TOKEN,
-      studioUrl: process.env.NUXT_SANITY_STUDIO_URL,
-    },
   },
 
   i18n: {
